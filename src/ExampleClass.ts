c503b7df3ac7331db2761200f7bc8dfbcfe6dc8d
Timothy Coleman <timhc22@gmail.com>
2022-04-11 17:27:48 +0100

    Add ExampleClass

--- src/ExampleClass.ts
+++ src/ExampleClass.ts
@@ -1,1 +1,21 @@
-export const ExampleClass = (name: string) => `Hello ${name}`;
+/**
+ * ExampleClass
+ */
+export default class ExampleClass {
+  private readonly name: string;
+
+  /**
+   * Constructor
+   * @param name
+   */
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  /**
+   * Say Hello
+   */
+  public sayHello = () => {
+    return `Hello ${this.name}`;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as ExampleClass } from './ExampleClass';
